89a4baeb34daee510df60c09fbb8218181149869
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-03 20:18:44 -0400

    Added most of the flow control.

--- logic.rb
+++ logic.rb
@@ -1,22 +1,57 @@
 require_relative 'card'
 require_relative 'deck'
 require_relative 'hand'
+require 'pry'
 
-# player_hand = Hand.new
-# dealer_hand = Hand.new
+def hit_or_stay
+end
 
-# apple = Deck.new
-# apple.shuffle
-# dealer_hand = Hand.new([apple.deal, apple.deal])
+def bust?(hand)
+  if hand > 21
+    true
+  else
+    false
+  end
+end
 
-# p dealer_hand.hand
-card1 = Card.new('A', '♠')
-card2 = Card.new('A', '♥')
+puts "Welcome to Blackjack!"
 
-card3 = Card.new('5', '♦')
-bill = Hand.new([card1, card2])
+deck_of_cards = Deck.new
 
-bill.add_card(card3)
+players_hand = Hand.new
+dealers_hand = Hand.new
 
-p bill.score
+p_card_1 = deck_of_cards.deal
+players_hand.add_card(p_card_1)
 
+d_card_1 = deck_of_cards.deal
+dealers_hand.add_card(d_card_1)
+
+d_card_2 = deck_of_cards.deal
+dealers_hand.add_card(d_card_2)
+
+puts "Player was delt #{p_card_1.show_card}"
+p_card_2 = deck_of_cards.deal
+players_hand.add_card(p_card_2)
+
+puts "Player was delt #{p_card_2.show_card}"
+puts "Players Score: #{players_hand.score}"
+
+puts "Hit or stay?"
+respond = gets.chomp.downcase
+while respond == 'h'
+  card = deck_of_cards.deal
+  players_hand.add_card(card)
+  puts "Player was delt #{card.show_card}"
+  puts "Players Score: #{players_hand.score}"
+  if bust?(players_hand.score)
+    puts "Bust!"
+     break
+  end
+  puts "Hit or stay?"
+  respond = gets.chomp.downcase
+end
+
+puts "Dealer was delt #{d_card_1.show_card}\nDealer was delt #{d_card_2.show_card}"
+
+puts "Dealer's Score: #{dealers_hand.score}"
